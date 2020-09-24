ba2b80a206297c34d2542fc3853235e28921d12a
Laura Cole <lauracole1900@comcast.net>
2020-09-24 17:17:09 -0600

    add variables

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables
+
+var passwordLength = 1;
+var lowerCase = true;
+var upperCase = true;
+var numberCase = true;
+var specialCase = true;
+var passwordOpt = {
+lCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+uCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+special = [" ", "!", "#", "%", "^", "*", "+", "-", "_", "?", ".", "~"]
+};
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
